dfd7d8beeffa030d1a6f5cb861ccd10d55e6ec3a
Larbi Djouama <larbidjouama@Larbis-Laptop.lan>
2024-02-27 00:38:18 +0000

    added questions

--- starter/index.js
+++ starter/index.js
@@ -10,6 +10,108 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const teamMembers=[];
 
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the team employee's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the employee's email?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "Finish building the team"],
+        name: "role",
+      },
+    ])
+    .then(function (answers) {
+      switch (answers.role) {
+        case "Manager":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is the manager's office number?",
+                name: "officeNumber",
+              },
+            ])
+            .then(function (managerAnswers) {
+              const manager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                managerAnswers.officeNumber
+              );
+              teamMembers.push(manager);
+              promptUser();
+            });
+          break;
+        case "Engineer":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is the engineer's GitHub username?",
+                name: "github",
+              },
+            ])
+            .then(function (engineerAnswers) {
+              const engineer = new Engineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                engineerAnswers.github
+              );
+              teamMembers.push(engineer);
+              promptUser();
+            });
+          break;
+        case "Intern":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What school does the intern attend?",
+                name: "school",
+              },
+            ])
+            .then(function (internAnswers) {
+              const intern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                internAnswers.school
+              );
+              teamMembers.push(intern);
+              promptUser();
+            });
+          break;
+        default:
+          // Finish building the team
+          const html = render(teamMembers);
+          fs.writeFile("./output/team.html", html, function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Team profile generated successfully!");
+            }
+          });
+      }
+    });
+}
+
+promptUser();
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,41 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name,id, email, gitHub) {
+        super(name,id,email)
+    //     this.name = name
+    //   this.id = id;
+    //   this.email = email;
+      this.gitHub = gitHub
+    }
+    getName(){
+        return this.name
+    }
+    getId(){
+        return this.id
+    }
+    getEmail(){
+        return this.email
+    }
+
+    getGithub(){
+        return this.gitHub
+    }
+    getRole(){
+
+
+
+        return Engineer;
+    }
+
+
+    }
+
+
+
+
+    module.exports = Engineer;
+
+
+
