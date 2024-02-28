9ec28309fcce0b4f93a9b8e1ab18477d835dd2d5
Larbi Djouama <larbidjouama@Larbis-Laptop.local>
2024-02-28 10:00:41 +0100

    added questions but option to add does not repeat

--- starter/index.js
+++ starter/index.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const { exit } = require("process");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 const teamMembers = [];
@@ -45,87 +46,122 @@ function promptUser() {
         answers.officeNumber
       );
       teamMembers.push(manager);
-      promptUser();
+      init();
+   
     })
+  }
 
-    .then(function (answers) {
-      inquirer.prompt([
-        {
-          type: "list",
-          message: "Which type of team member would you like to add?",
-          choices: ["Engineer", "Intern", "Finish building the team"],
-          name: "role",
-        },
-      ])
-
-     
-            .then(function (answers) {
+  function init(){
+  inquirer.prompt([
+              {
+                type: "list",
+                  message: "Which type of team member would you like to add?",
+                  choices: ["Engineer", "Intern", "Finish building the team"],
+                name: "role",
+              },
+            ]).then(function (answers) {
               switch (answers.role) {
                 case "Engineer":
-                  inquirer
-                    .prompt([
-                      {
-                        type: "input",
-                        message: "What is the engineer's GitHub username?",
-                        name: "github",
-                      },
-                    ])
-              const engineer = new Engineer(
-                answers.name,
-                answers.id,
-                answers.email,
-                answers.github
-              );
-              teamMembers.push(engineer);
-              inquirer.prompt([
-                {
-                  type: "list",
-                  message: "Which type of team member would you like to add?",
-                  choices: ["Engineer", "Intern", "Finish building the team"],
-                  name: "role",
-                },
-              ]);
-            });
-          break;
-        case "Intern":
+                  addEngineer();
+                  break;
+                case "Intern":
+                  addIntern();
+                  break;
+                case "Finish building the team":
+                  buildTeam();
+                  break;
+                  
+                
+              }
+            }
+          )
+        };
+
+
+        function addEngineer(){
           inquirer
-            .prompt([
+          .prompt([
+            
               {
                 type: "input",
-                message: "What school does the intern attend?",
-                name: "school",
+                message: "What is the engineer's name?",
+                name: "name",
               },
-            ])
-            .then(function (answers) {
-              const intern = new Intern(
-                answers.name,
-                answers.id,
-                answers.email,
-                internAnswers.school
-              );
-              teamMembers.push(intern);
-              inquirer.prompt([
-                {
-                  type: "list",
-                  message: "Which type of team member would you like to add?",
-                  choices: ["Engineer", "Intern", "Finish building the team"],
-                  name: "role",
-                },
-              ]);
-            });
-          break;
-        default:
-          // Finish building the team
-          const html = render(teamMembers);
-          fs.writeFile("./output/team.html", html, function (err) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("Team profile generated successfully!");
-            }
-          });
-      }
-    });
-}
+              {
+                type: "input",
+                message: "What is the engineer's ID?",
+                name: "id",
+              },
+              {
+                type: "input",
+                message: "What is the engineer's email?",
+                name: "email",
+              },
+              {
+                type: "input",
+                message: "What is the engineer's GitHub username?",
+                name: "github",
+              },
 
-promptUser();
+          
+        ])
+        .then(function (answers) {
+
+        const engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        teamMembers.push(engineer);
+      })};
+
+      function addIntern (){
+        inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is the Intern's name?",
+            name: "name",
+          },
+          {
+            type: "input",
+            message: "What is the Intern's ID?",
+            name: "id",
+          },
+          {
+            type: "input",
+            message: "What is the Intern's email?",
+            name: "email",
+          },
+          {
+            type: "input",
+            message: "What school did the Intern attend ?",
+            name: "school",
+          },
+
+        ])
+        .then(function (answers) {
+          const intern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.school
+          );
+          teamMembers.push(intern);
+        });
+      };
+
+      function buildTeam(){
+        const html = render(teamMembers);
+        fs.writeFile("./output/team.html", html, function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Team profile generated successfully!");
+          }
+        });
+      }
+        
+
+        promptUser();
